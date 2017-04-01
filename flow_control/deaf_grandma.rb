9273230132398ee10b124b08a2c5a34d5cf32e89
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-01 17:23:28 +0100

    Added angry_boss program, deaf_grandma program, and more

---
+++ flow_control/deaf_grandma.rb
@@ -0,0 +1,21 @@
+# deaf grandma
+
+
+def deaf_grandma
+  puts "HUH?"
+  answer = gets.chomp
+    while !answer.empty?
+      exit if answer == "BYE"
+    if answer == answer.upcase
+      year_gen = rand(1930..1950)
+      puts "NO, NOT SINCE #{year_gen}!"
+      answer = gets.chomp
+    else answer == answer.downcase
+      puts "HUH?! SPEAK UP, SONNY!"
+      answer = gets.chomp
+    end
+  end
+end
+
+
+deaf_grandma
