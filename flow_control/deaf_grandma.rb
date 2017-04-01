6647c4ac3c6a34eeb26021d26895011f5213f65f
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-04-02 00:44:37 +0100

    Completed recursion fundamentals, builing and sorting arrays program

--- flow_control/deaf_grandma.rb
+++ flow_control/deaf_grandma.rb
@@ -4,8 +4,8 @@
 def deaf_grandma
   puts "HUH?"
   answer = gets.chomp
-    while !answer.empty?
-      exit if answer == "BYE"
+  while !answer.empty?
+    exit if answer == "BYE"
     if answer == answer.upcase
       year_gen = rand(1930..1950)
       puts "NO, NOT SINCE #{year_gen}!"
